fc41325062c35f659a07b1031078e29401c238b8
dmitry@orangelv.com <dmitry@orangelv.com>
2025-10-16 06:59:39 +0300

    latest files for notification release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.nordpool1hprices"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.8.5"
+        versionCode = 7
+        versionName = "1.8.3"
     }
 
     // Build commands:
